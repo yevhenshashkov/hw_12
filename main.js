7f19ef11405853dfeb662433976bfccc9b07d6dd
YevhenShashkov <tehnolog3000@gmail.com>
2025-03-24 20:52:44 +0200

    sec

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
-const participantsList = document.querySelector("#partisipantsList");
-const addNewInput = document.querySelector("#new-part");
+const participantsList = document.querySelector("#tasksList");
+const addNewInput = document.querySelector("#new-task");
 const addNewButton = document.querySelector("#addNew");
 
-
-function addParticipant(name) {
+function addNew(name) {
     const li = document.createElement("li");
     li.innerHTML = `${name} <button class="deleteBtn">x</button>`;
     participantsList.appendChild(li);
 }
 
+const participantArr = [git push origin main
+    "Навчитися запам'ятовувати код JS",
+    "Зробити ДЗ",
+    "Погуглити додаткову інформацію"
+];
 
-const participantArr = [
-    "Bob",
-    "Alice",
-    "Nick"];
-participantArr.forEach(addParticipant);
-
+participantArr.forEach(addNew); 
 
 participantsList.addEventListener("click", (e) => {
     if (e.target.classList.contains("deleteBtn")) {
@@ -25,9 +24,8 @@ participantsList.addEventListener("click", (e) => {
 
 addNewButton.addEventListener("click", () => {
     const name = addNewInput.value.trim();
-
     if (name) {
-        addParticipant(name);
-        addNewInput.value = "";
+        addNew(name);
+        addNewInput.value = ""; 
     }
 });
